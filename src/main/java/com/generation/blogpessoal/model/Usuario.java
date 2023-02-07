1180f6260a65ad109c124df929743c656d7302cb
williamkolosque <william.maich@mercadolivre.com>
2023-02-07 14:12:40 -0300

    criado security e login

---
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -0,0 +1,85 @@
+package com.generation.blogpessoal.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name="tb_usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O atributo nome é Obrigatório")
+    private String nome;
+
+    @NotNull(message = "O atributo Usuário é Obrigatório")
+    @Email(message = "O atributo Usuário deve ser um email valido")
+    private String usuario;
+
+    @NotBlank(message = "O Atributo senha é obrigatório")
+    @Size(min = 8, message = "A senha deve conter no minimo 8 caracteres")
+    private String senha;
+
+    @Size(max = 5000, message = "O link da foto não pode conter mais do que 5000 caracteres")
+    private String foto;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("usuario")
+    private List<Postagem> postagem;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public List<Postagem> getPostagem(){
+        return this.postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
+    }
+}
