5a6033e5934ee26e555e5870d4c64e92af385073
williamkolosque <william.maich@mercadolivre.com>
2023-02-13 11:49:21 -0300

    criado os testes de usuario

---
+++ src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.generation.blogpessoal.repository;
+
+import com.generation.blogpessoal.model.Usuario;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeAll
+    void start(){
+
+        usuarioRepository.deleteAll();
+
+        usuarioRepository.save(new Usuario(0l,"João da silva","joao@email.com.br","12345678","https://i.imgur.com/FETvs20.jpg"));
+
+        usuarioRepository.save(new Usuario(0l,"Manuela da Silva","manuela@email.com.br","12345678","https://i.imgur.com/FETvs20.jpg"));
+
+        usuarioRepository.save(new Usuario(0l,"adriana melo rego silva","adriana@email.com.br","12345678","https://i.imgur.com/FETvs20.jpg"));
+
+        usuarioRepository.save(new Usuario(0l,"Paula tejano","paula@email.com.br","12345678","https://i.imgur.com/FETvs20.jpg"));
+
+
+    }
+
+    @Test
+    @DisplayName("Retorna 1 usuario")
+    public void deveRetornaUmUsuario(){
+        Optional<Usuario> usuario = usuarioRepository.findByUsuario("joao@email.com.br");
+        assertTrue(usuario.get().getUsuario().equals("joao@email.com.br"));
+    }
+
+
+    @Test
+    @DisplayName("Retorna 3 usuarios")
+    public void dveRetornarTresUsuarios(){
+        List<Usuario> listaDeUsuarios = usuarioRepository.findAllByNomeContainingIgnoreCase("Silva");
+        assertEquals(3, listaDeUsuarios.size());
+        assertTrue(listaDeUsuarios.get(0).getNome().equals("João da silva"));
+        assertTrue(listaDeUsuarios.get(1).getNome().equals("Manuela da Silva"));
+        assertTrue(listaDeUsuarios.get(2).getNome().equals("adriana melo rego silva"));
+    }
+
+    @AfterAll
+    public void end(){
+        usuarioRepository.deleteAll();
+    }
+
+
+}
